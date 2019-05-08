964f3f58fedd99bb7ae9dfcb60933293277b567f
sbasile-ch <sbasile@companieshouse.gov.uk>
2019-05-08 12:58:02 +0100

    fixed relaxed match with strict equals

--- auto_ssh.sh
+++ auto_ssh.sh
@@ -9,7 +9,7 @@ function get_column {
     local C1=$1
     local C2=$2
     local col=$3
-    OPTIONS=$(cat ${AUTO_SSH_DIR}/known_hosts | awk -v C1=$C1 -v C2=$C2 -v c=$col 'BEGIN{FS=","} {if(NF>=7 && match($1,C1) && match($2,C2)){print $c}}' | sort -u)
+    OPTIONS=$(cat ${AUTO_SSH_DIR}/known_hosts | tr -d '[:blank:]' | awk -v C1="$C1" -v C2="$C2" -v c="$col" 'BEGIN{FS=","} {if(NF>=7 && (C1=="" || ($1==C1 && (C2=="" || $2==C2)))){print $c}}' | sort -u)
 }
 #--------------------------------------
 function sep {
